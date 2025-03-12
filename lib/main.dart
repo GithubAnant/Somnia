c1fe22cb12b00378056e012545ec484febe72273
Anant Singhal <anantsinghal444@gmail.com>
2025-03-13 00:30:35 +0530

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:somnia/write.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main(){
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(Somnia());
 }
+
 
 class Somnia extends StatelessWidget {
   const Somnia({super.key});
